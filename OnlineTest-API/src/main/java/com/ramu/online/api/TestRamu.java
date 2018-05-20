ae3c0108c6d650e7e0381d09d8f22c69ddc12677
Ramu <Ramu@Ramu>
2018-05-20 16:28:02 +0530

    New File added

---
+++ OnlineTest-API/src/main/java/com/ramu/online/api/TestRamu.java
@@ -0,0 +1,17 @@
+package com.ramu.online.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ramu
+ *
+ *         20-May-2018
+ */
+@SpringBootApplication
+public class TestRamu {
+	public static void main(String[] args) {
+		SpringApplication.run(TestRamu.class, args);
+	}
+
+}
